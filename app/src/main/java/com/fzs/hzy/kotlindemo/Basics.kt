926ae7098a320380a0d4e162d1f62c55503537c0
hzyxym <953624084@qq.com>
2018-12-18 00:07:42 +0800

    for语法

--- app/src/main/java/com/fzs/hzy/kotlindemo/Basics.kt
+++ app/src/main/java/com/fzs/hzy/kotlindemo/Basics.kt
@@ -1,13 +1,19 @@
 package com.fzs.hzy.kotlindemo
 
 import com.fzs.hzy.baselibrary.utils.*
+
 val PI = 3.14
 var x = 0
 fun main(vararg args: String) {
+//    basics()
+    printForeach()
+}
+
+fun basics(){
     println("sum:${sum(12, 10)}")
     printSum(2, 3)
     var a = 2
-    var b : Int = getValue()
+    var b: Int = getValue()
     printSum(a, b)
 
     println("a++ is ${a++}")
@@ -16,8 +22,28 @@ fun main(vararg args: String) {
     println("x changeX is ${changeX()}")
     x += 3
     println("x: $x")
+    println(maxOf(5, 4))
+    println(parseInt("q"))
+    printProduct("12","v")
+    println("length: ${getStringLength(1)}")
 }
 
-fun getValue():Int{
-    return 2
+fun printForeach(){
+    val list = listOf<String>("a","b","c")
+    for(it in list){
+        print("$it ")
+    }
+
+    println()
+    val fruitList = listOf("apple","banana","pear")
+    for (index in fruitList.indices){
+        println("item at $index is ${fruitList[index]}")
+    }
+
+    fruitList.forEach { println("$it") }
+    println(fruitList.last { it.equals("banana") })
 }
+
+
+
+
--- baselibrary/src/main/java/com/fzs/hzy/baselibrary/utils/CaculateUtils.kt
+++ baselibrary/src/main/java/com/fzs/hzy/baselibrary/utils/CaculateUtils.kt
@@ -6,10 +6,42 @@ fun printSum(a: Int, b: Int) {
     println("sum of $a and $b is ${a + b}")
 }
 
-fun maxOf(a: Int, b: Int): Int {
+fun maxOfSimple(a: Int, b: Int): Int {
     return if(a > b){
         a
     }else {
         b
     }
+}
+
+fun maxOf(a :Int,b: Int) = if(a > b) a else b
+
+fun parseInt(str : String): Int?{
+    return try {
+        val i = Integer.parseInt(str)
+        i
+    } catch (e: Exception) {
+        null
+    }
+}
+
+fun getValue(): Int {
+    return 2
+}
+
+fun printProduct(str1: String, str2: String) {
+    val x = parseInt(str1)
+    val y = parseInt(str2)
+    if (x != null && y != null) {
+        println("x*y=${x * y}")
+    } else {
+        println("either '$str1' or '$str2' is not a number")
+    }
+}
+
+fun getStringLength(obj : Any): Int?{
+    if(obj is String){
+        return obj.length
+    }
+    return null
 }
