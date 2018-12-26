01241a642b910ca504e445dd5b97692e5970f8ba
hzy <huangzhenying@phone580.com>
2018-12-26 18:25:59 +0800

    闪屏功能

--- app/src/main/java/com/fzs/hzy/kotlindemo/ControlFlow.kt
+++ app/src/main/java/com/fzs/hzy/kotlindemo/ControlFlow.kt
@@ -1,10 +1,14 @@
 package com.fzs.hzy.kotlindemo
 
+import java.security.SecureRandom
+import kotlin.random.Random
+
 fun main(vararg args: String) {
     println("control flow")
 //    println(getMaxValue(4,9))
-    println(ifGarmmar(32, 2))
-    when2Grammar(1)
+//    println(ifGarmmar(32, 2))
+//    when2Grammar(1)
+    randomGrammar()
 }
 
 fun getMaxValue(a: Int, b: Int): Int {
@@ -27,4 +31,32 @@ fun when2Grammar(x : Any) = when (x) {
     else -> { // 注意这个块
         print("x is neither 1 nor 2")
     }
+}
+
+fun randomGrammar(){
+    var r = SecureRandom()
+    println(r.nextFloat())
+    println(r.nextFloat())
+    println(r.nextFloat())
+    println(r.nextFloat())
+    println(r.nextFloat())
+    println(r.nextFloat())
+    println(r.nextFloat())
+    println(r.nextFloat())
+    println(r.nextFloat())
+    println(r.nextFloat())
+    println(r.nextFloat())
+    println(r.nextFloat())
+    println(r.nextFloat())
+    println(r.nextFloat())
+    println(r.nextFloat())
+//    println(r.nextInt(3))
+
+//    var random = Random(255)
+//    println(random.nextInt())
+//    println(random.nextInt())
+//    println(random.nextInt())
+
+//    val random = (Math.random() * 5).toInt()
+//    println(random)
 }
